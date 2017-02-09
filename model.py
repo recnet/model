31495a12e431d40dd0a67820110e51ec4e16a455
jaxing <jaxing@student.chalmers.se>
2017-02-09 13:52:10 +0100

    Makes new checkpoint instead of overwriting

--- model.py
+++ model.py
@@ -1,13 +1,15 @@
 import tensorflow as tf
 import os.path
 
-checkpoint_path = "checkpoints/model.ckpt"
+checkpoint_version = 0
+checkpoint_path = "checkpoints/model"+str(checkpoint_version)+".ckpt"
 init_op = tf.global_variables_initializer()
 saver = tf.train.Saver()
 
 with tf.Session() as sess:
     if os.path.isfile(checkpoint_path):
         saver.restore(sess, checkpoint_path)
+        checkpoint_version = checkpoint_version + 1
     else:
         sess.run(init_op)
 
