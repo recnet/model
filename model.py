9b3e2e631222097aad4c9d96a6b7f3578516b64c
Alexander Håkansson <al3xand3r.h@gmail.com>
2017-02-09 13:45:33 +0100

    Loads saved model if it exists

--- model.py
+++ model.py
@@ -1,12 +1,18 @@
 import tensorflow as tf
+import os.path
 
+checkpoint_path = "checkpoints/model.ckpt"
 init_op = tf.global_variables_initializer()
 saver = tf.train.Saver()
 
 with tf.Session() as sess:
+    if os.path.isfile(checkpoint_path):
+        saver.restore(sess, checkpoint_path)
+    else:
         sess.run(init_op)
-        # Do work ...
 
-        # Save model when done
-        save_path = saver.save(sess, "checkpoints/model.ckpt")
-        print("Model saved in: %s" % save_path)
+    # Do work ...
+
+    # Save model when done
+    save_path = saver.save(sess, checkpoint_path)
+    print("Model saved in: %s" % save_path)
