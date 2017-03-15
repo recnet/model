1368b9943ee3b187f3cb4157ad4409c5a648294d
Maxim Goretskyy <13maxim13@live.se>
2017-03-15 14:52:37 +0100

    Creates writer for logging

--- src/writer.py
+++ src/writer.py
@@ -1,7 +1,20 @@
-class Writer:
-    def write(self, config):
-        if 'logfile' in config:
-            for key in config:
-                print("key: ", key, " value: ", config[key])
+from definitions import LOGS_DIR
+import os
+
+def log_config(config):
+    filename = 'README.txt'
+    type = config['type']
+    name = config['name']
+    dir_to_create = LOGS_DIR + '/' + type + '/' + name
+    result = ''
+    for key in config:
+        result += "key: " + str(key) + "\tvalue: " + str(config[key]) + "\n"
+
+    if not os.path.exists(dir_to_create):
+        raise FileNotFoundError('Can not write because no directory is created')
+
+    with open(dir_to_create+'/'+filename, "w+") as f:
+        f.write(result)
+
 
 
