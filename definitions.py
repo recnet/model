23f035559589f09e4169ef73ee99df23b27880bd
Maxim Goretskyy <13maxim13@live.se>
2017-03-16 10:58:30 +0100

    Adds directory for checkpoints for each network

--- definitions.py
+++ definitions.py
@@ -3,11 +3,9 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATASETS_PATH = os.path.join(ROOT_DIR, "resources/datasets")
-
-CHECKPOINTS_PATH = os.path.join(ROOT_DIR, "checkpoints")
-
 LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 
 TENSOR_DIR_TRAIN = 'tensorDir/train'
 TENSOR_DIR_VALID = 'tensorDir/valid'
+CHECKPOINTS_DIR = 'checkpoints'
 
