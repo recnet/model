599390470804964d3eb6d0fac74d1deb36132b1f
Maxim Goretskyy <13maxim13@live.se>
2017-03-31 19:10:44 +0200

    Cleans, removes unused function, removes unused headers

--- model/util/writer.py
+++ model/util/writer.py
@@ -25,69 +25,20 @@
 import os
 import csv
 import time
-from definitions import LOGS_DIR, LOGGING_RESULTS_FILE
-
-def log_config(config):
-    filename = 'README.txt'
-    networktype = config['type']
-    name = config['name']
-    dir_to_create = LOGS_DIR + '/' + networktype + '/' + name
-    result = ''
-    for key in config:
-        result += "key: " + str(key) + "\tvalue: " + str(config[key]) + "\n"
-
-    if not os.path.exists(dir_to_create):
-        raise FileNotFoundError('Can not write because no directory is created')
-
-    with open(dir_to_create+'/'+filename, "w+") as f:
-        f.write(result)
+from definitions import *
 
 
-def log_samefile(config, f1_score, epoch_top, prec_valid, prec_train, recall_valid, recall_train):
-    NET_TYPE = 'type'
-    NET_NAME = 'name'
-    VOC_SIZE = 'vocabulary_size'
-    USER_COUNT = 'user_count'
-    LEARN_RATE = 'learning_rate'
-    EMBEDD_SIZE = 'embedding_size'
-    MAX_TITLE_LENGTH = 'max_title_length'
-    LSTM_NEURONS = 'lstm_neurons'
-    HIDDEN_NEURONS = 'hidden_neurons'
-    BATCH_SIZE = 'batch_size'
-    TRAINING_EPOCHS = 'training_epochs'
-    USERS_TO_SELECT ='users_to_select'
-    USE_L2_LOSS = 'use_l2_loss'
-    L2_FACTOR = 'l2_factor'
-    USE_DROPOUT = 'use_dropout'
-    DROPOUT_PROB = 'dropout_prob'
-    USE_CONSTANT_LIMIT = 'use_constant_limit'
-    CONSTANT_PREDICTION_LIMIT = 'constant_prediction_limit'
-    TRAINABLE_MATRIX = 'trainable_matrix'
-    PRE_TRAINED_DIMENSION = 'pre_trained_dimension'
-    PRE_TRAINED_MATRIX = 'pre_trained_matrix'
-    USE_PRETRAINED = 'use_pretrained'
-    VALIDATION_DATA = 'validation_data'
-    TRAINING_DATA = 'training_data'
-    TESTING_DATA = 'testing_data'
-
-    F1_SCORE_TOP = 'F1 Score highest'
-    EPOCH_WHEN_F1_TOP = 'Epoch for highest F1'
-    PRECISION_VALIDATION = 'Precision validation'
-    PRECISION_TRAINING = 'Precision training'
-    RECALL_VALIDATION = 'Recall validation'
-    RECALL_TRAINING = 'Recall training'
-    DATE = 'Date of experiment'
-
+def log_samefile(config, f1_score_valid, f1_score_train, epoch_top, prec_valid, prec_train, recall_valid, recall_train):
     filename = LOGGING_RESULTS_FILE
     if not os.path.exists(LOGS_DIR):
         raise FileNotFoundError('Can not write because no directory is created')
 
     config_headers = [NET_TYPE, NET_NAME, VOC_SIZE, USER_COUNT, LEARN_RATE, EMBEDD_SIZE, MAX_TITLE_LENGTH, LSTM_NEURONS,
-               HIDDEN_NEURONS, BATCH_SIZE, TRAINING_EPOCHS, USERS_TO_SELECT, USE_L2_LOSS, L2_FACTOR, USE_DROPOUT,
-               DROPOUT_PROB, USE_CONSTANT_LIMIT, CONSTANT_PREDICTION_LIMIT, TRAINABLE_MATRIX, PRE_TRAINED_DIMENSION,
+               HIDDEN_NEURONS, BATCH_SIZE, TRAINING_EPOCHS, USE_L2_LOSS, L2_FACTOR, USE_DROPOUT,
+               DROPOUT_PROB, USE_CONSTANT_LIMIT, CONSTANT_PREDICTION_LIMIT, TRAINABLE_MATRIX,
                PRE_TRAINED_MATRIX, USE_PRETRAINED, VALIDATION_DATA, TRAINING_DATA, TESTING_DATA]
 
-    additional_headers = [F1_SCORE_TOP, EPOCH_WHEN_F1_TOP, PRECISION_VALIDATION, PRECISION_TRAINING,
+    additional_headers = [F1_SCORE_TOP_VALID, F1_SCORE_TRAIN, EPOCH_WHEN_F1_TOP, PRECISION_VALIDATION, PRECISION_TRAINING,
                           RECALL_VALIDATION, RECALL_TRAINING, DATE]
 
     headers = config_headers + additional_headers
@@ -102,7 +53,9 @@ def log_samefile(config, f1_score, epoch_top, prec_valid, prec_train, recall_val
         data.append(config[header])
 
     time_logged = time.strftime("%c")
-    data.append(str(f1_score))
+    #Order of appends must match that of headers.
+    data.append(str(f1_score_valid))
+    data.append(str(f1_score_train))
     data.append(str(epoch_top))
     data.append(str(prec_valid))
     data.append(str(prec_train))
@@ -113,5 +66,3 @@ def log_samefile(config, f1_score, epoch_top, prec_valid, prec_train, recall_val
     with open(filename, 'a+', newline='') as fp:
         a = csv.writer(fp, delimiter=',')
         a.writerow(data)
-
-# log_samefile(None, None, None, None, None, None, None)
